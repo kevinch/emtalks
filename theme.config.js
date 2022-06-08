b6f47c2238a39709782da8531bcee0b83e9fbd02
kevin chevallier <kevin.chevallier@vtex.com.br>
2022-06-08 10:16:04 -0300

    Initial commit

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © Kevin Chevallier.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
@@ -17,5 +17,10 @@ export default {
         }
       `}</style>
     </small>
-  )
+  ),
+  readMore: 'Read More →',
+  darkMode: true,
+  nextLinks: true,
+  prevLinks: true,
+  search: true
 }
