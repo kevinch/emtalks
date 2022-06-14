7cfbd3e8acab4ab934aafdbe966dd10afddceb89
kevin <kevinchevallier@gmail.com>
2022-06-14 10:35:39 -0300

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 
 import '../styles/main.css'
 
@@ -19,6 +21,24 @@ export default function Nextra({ Component, pageProps }) {
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
+        />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+          }}
         />
       </Head>
       <Component {...pageProps} />
