96dc716e2667b31f421788532b0ccd30f1018069
kevinch <kevinchevallier@gmail.com>
2024-01-24 09:50:23 -0300

    Remvoe gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-import Script from 'next/script'
-import * as gtag from '../lib/gtag'
 
 import '../styles/main.css'
 
@@ -23,24 +21,6 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-        }}
-      />
       <Component {...pageProps} />
     </>
   )
